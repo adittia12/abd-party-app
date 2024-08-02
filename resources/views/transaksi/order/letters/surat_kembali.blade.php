e6c2d46e02e3aba493540c12adba3375591bad54
adittia12 <agustianaa12adit@gmail.com>
2024-08-02 10:46:53 +0700

    FEAT : Update Letters

--- resources/views/transaksi/order/letters/surat_kembali.blade.php
+++ resources/views/transaksi/order/letters/surat_kembali.blade.php
@@ -48,6 +48,9 @@
                     <div>
                         <p class="text-info-jalan">Kepada : <b>{{ $cetakOrder->name_customer }}</b> <br>
                             {{ $cetakOrder->delivery_address }}
+                            @if ($cetakOrder->no_phone)
+                                <br>Nomor HP : <b>0{{ $cetakOrder->no_phone }}</b>
+                            @endif
                         </p>
                         <input type="hidden" name="id" value="{{ $cetakOrder->id }}">
                         <div class="section-title "><b class="text-info-jalan">Surat Kembali</b></div>
